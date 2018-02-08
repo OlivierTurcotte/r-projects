d954ec73917304e6bbb88379bafca5b1c380cecf
Olivier Turcotte <olivier.turcotte.18@ulaval.ca>
2018-02-07 20:07:25 -0500

    Ajout du numéro 2.3.3 (dépannage) au documenet Dépannage3.R

--- Olivier/Intro II/Dépannage 3.R
+++ Olivier/Intro II/Dépannage 3.R
@@ -69,3 +69,34 @@ o <- sum(1/lambda^2)/n^2
 plot(Vectorize(fx),0,1000)
 curve(dnorm(x,µ,sqrt(o)),0,1000,add = T, col = "red")
 
+
+# 2.3.3 ----
+
+# Initialisation
+m <- 1000000
+set.seed(20160419)
+U <- matrix(runif(m*3),ncol = 3, byrow = T)
+X <- matrix(c(rlnorm(m,log(10)-0.18,0.6),rexp(m,0.1),rgamma(m,2,1/5)),ncol = 3)
+S <- apply(X,1,sum)
+kappa <- c(0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999)
+
+# e)
+
+sorted_X <- apply(X,2,sort)
+esperance_X <- apply(X,2,mean)
+variance_X <- apply(X,2,function(x) mean(x^2)) - esperance_X^2
+VaR_X <- sorted_X[m*kappa,]
+TVaR_X <- sapply(seq(kappa),function(i) apply(sorted_X[(m*kappa[i]+1):m, ],2,mean))
+
+# f)
+
+sorted_S <- sort(S)
+esperance_S <- mean(S)
+variance_S <- mean(S^2)-esperance_S^2
+VaR_S <- sorted_S[m*kappa]
+TVaR_S <- sapply(seq(kappa), function(i) mean(sorted_S[(m*kappa[i]+1):m]))
+# g)
+BM <- sapply(seq(kappa),function(i) sum(TVaR_X[,i])-TVaR_S[i])
+BM
+
+
