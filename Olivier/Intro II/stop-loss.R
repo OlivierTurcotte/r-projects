42c4b2c40f74f176e53eb47447cc8a4a856c8c78
Olivier Turcotte <olivier.turcotte.18@ulaval.ca>
2018-02-09 09:36:36 -0500

    MAJ de la fonction stopLoss pour permettre de définir dynamiquement le paramètre N

--- Olivier/Intro II/stop-loss.R
+++ Olivier/Intro II/stop-loss.R
@@ -1,13 +1,24 @@
 
 # Fonction générale de calcul de la stop-loss
 # pour des densités discrète.
-stopLoss <- function(d, density.exp,n){
+stopLoss <- function(d, density.exp){
     density <- substitute(density.exp)
+    # Idée, sommer test jusqu'à ce que ce soit égale à 1, puis prendre le nombre d'éléments
+    n <- 0
+    density$x <- n
+    s <- eval(density)
+    while(s < 1){
+        n <- n+1 
+        density$x <- n
+        s <- s + eval(density)
+    }
     density$x <- (d+1):n
     sum(eval(density)*seq_len(n-d))
 }
 
 # Exemples
-stopLoss(2,dbinom(4,0.5),4)
-stopLoss(20,dnbinom(5,0.5),1000)
+stopLoss(2,dbinom(4,0.5))
+stopLoss(20,dnbinom(5,0.5))
+
+
 
